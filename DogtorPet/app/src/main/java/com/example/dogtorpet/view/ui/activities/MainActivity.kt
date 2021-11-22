48f5fc015991f6dd2202884ed33ea9b9b21a91d9
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2021-11-22 15:50:06 -0500

    nav_bar dogtor pet
    el img button de el logo no funciona
    arreglar el padding en los botnmes del nav bar

--- DogtorPet/app/src/main/java/com/example/dogtorpet/view/ui/activities/MainActivity.kt
+++ DogtorPet/app/src/main/java/com/example/dogtorpet/view/ui/activities/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.dogtorpet.view.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.dogtorpet.R
+import com.example.dogtorpet.databinding.ActivityMainBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import android.graphics.drawable.GradientDrawable
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navView: BottomNavigationView = binding.navView
+
+        navView.background = null
+
+        val botView: BottomAppBar = binding.bottomappbar
+        botView.setBackgroundColor(Color.parseColor("#C5F9F9"))
+        
+        //Color.parseColor("#C5F9F9")
+
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_settings, R.id.navigation_account
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
     }
 }
