67536b557e4e22a8ee72a040924f560880924bf9
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2021-11-24 20:20:35 -0500

    product cards added

---
+++ DogtorPet/app/src/main/java/com/example/dogtorpet/view/ui/activities/productsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dogtorpet.view.ui.activities
+
+
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dogtorpet.databinding.ActivityMainBinding
+
+
+
+
+class productsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
+
